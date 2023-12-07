42fc102a68cf99a5745e385fede70bf4503aa041
Malak Fora <2067247@bdeb.qc.ca>
2023-12-07 15:00:51 -0500

    J'ai creer les binding command pour l'ouverture des fenetres

--- View/MainWindow.xaml.cs
+++ View/MainWindow.xaml.cs
@@ -20,5 +20,73 @@ namespace View
         {
             InitializeComponent();
         }
+
+        public static RoutedCommand ChangerUserCmd = new RoutedCommand();
+
+        private void ChangerUser_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = true;
+        }
+
+        private void ChangerUser_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            choixUtilisateur choixUtilisateur = new choixUtilisateur();
+            choixUtilisateur.Owner = this;
+            choixUtilisateur.ShowDialog();
+        }
+
+        public static RoutedCommand CommanderLivreCmd = new RoutedCommand();
+
+        private void CommanderLivre_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = true;
+        }
+
+        private void CommanderLivre_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            CommandeLivre commandeLivre = new CommandeLivre();
+            commandeLivre.Owner = this;
+            commandeLivre.ShowDialog();
+        }
+
+        public static RoutedCommand OuvrirAdminCmd = new RoutedCommand();
+
+        private void OuvrirAdmin_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = true;
+        }
+
+        private void OuvrirAdmin_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            FenetreAdmin fenetreAdmin = new FenetreAdmin();
+            fenetreAdmin.Owner = this;
+            fenetreAdmin.ShowDialog();
+        }
+
+        public static RoutedCommand QuitterCmd = new RoutedCommand();
+
+        private void Quitter_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = true;
+        }
+
+        private void Quitter_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+           this.Close();
+        }
+
+        public static RoutedCommand TransfererLivreCmd = new RoutedCommand();
+
+        private void TransfererLivre_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = true;
+        }
+
+        private void TransfererLivre_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            choixUtilisateur choixUtilisateur = new choixUtilisateur();
+            choixUtilisateur.Owner = this;
+            choixUtilisateur.ShowDialog();
+        }
     }
 }
